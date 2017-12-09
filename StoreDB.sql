c7acc0378e5dd335ab3bd8ff377203e86a9f28e2
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-09 23:44:56 +0300

    new DB

--- StoreDB.sql
+++ StoreDB.sql
@@ -1,11 +1,41 @@
-create database SHOP;
+create database StoreDB;
+use storeDB;
 
-use SHOP;
+select * from producer;
+insert into producer(name_producer, country,city) values ('Дняпроўскi збан','РБ','Гомель');
+insert into producer(name_producer, country,city) values ('Идеал','РБ','Минск');
+insert into producer (name_producer, country,city) values ('Кстати','РБ','Гродно');
+insert into producer(name_producer, country,city) values ('Бабушкины деликатесы','РБ','Могилёв');
+insert into producer(name_producer, country,city) values ('Белорусская кофейня','РБ','Витебск');
+insert into producer(name_producer, country,city) values ('Вкус рыбы','РБ','Речица');
+insert into producer(name_producer, country,city) values ('Коммунарка','РБ','Минск');
+insert into producer(name_producer, country,city) values ('Трайпл','РБ','Брест');
+insert into producer(name_producer, country,city) values ('МяскоВит','РБ','Минск');
+insert into producer(name_producer, country,city) values ('ТворожкоМорожко','РФ','Смоленск');
+insert into producer(name_producer, country,city) values ('Хлебный Дар','РФ','Москва');
+insert into producer(name_producer, country,city) values ('Ферма','РБ','Минская область');
+insert into producer(name_producer, country,city) values ('Хозяйственная Ферма','РФ','Московская область');
+insert into producer(name_producer, country,city) values ('Соусный мир','Украина','Киев');
 
-select * from products
- insert into products (name_product, price, id_producer,id_cat, description_product) values ('Молоко', 100, 1,0, 'tasty');
- insert into categories (id_cat, name_cat) values (0, 'молочные продукты');
- insert into producer (name_producer, country) values ('Russian Milk', 'Russia');
+select * from categories;
+
+insert into categories(id_cat, name_cat) values(0, 'молочные продукты');
+insert into categories(id_cat, name_cat) values(1, 'соки,напитки');
+insert into categories(id_cat, name_cat) values(2, 'мясо');
+insert into categories(id_cat, name_cat) values(3, 'рыба');
+insert into categories(id_cat, name_cat) values(4, 'чай, кофе');
+insert into categories(id_cat, name_cat) values(5, 'конфеты');
+insert into categories(id_cat, name_cat) values(6, 'печенье');
+insert into categories(id_cat, name_cat) values(7, 'зерновые');
+insert into categories(id_cat, name_cat) values(8, 'фрукты');
+insert into categories(id_cat, name_cat) values(9, 'овощи');
+insert into categories(id_cat, name_cat) values(11, 'соусы');
+insert into categories(id_cat, name_cat) values(12, 'хлебобулочные изделия');
+
+
+select * from products;
+insert into products(name_product,price, id_producer, id_cat, description_product,ammount) values('Хлеб', 1, 11,12, 'Нормальный хлеб', 50);
+
 create table products
 (
 id_product int Identity(1,1) primary key,
@@ -13,33 +43,27 @@ name_product varchar(100),
 price double precision,
 id_producer int,
 id_cat int,
-description_product varchar(100)
+description_product varchar(100),
+ammount int
 );
 
 create table producer
 (
 id_producer int Identity(1,1) primary key,
 name_producer varchar(100),
-country varchar(100) 
+country varchar(100),
+city varchar(20) 
 );
-
-create table categories
-(
-id_cat int primary key,
-name_cat varchar(100)
-);
-
-drop table employee;
 
 create table employee
 (
 id_employee int Identity(1,1) primary key,
 name_employee varchar(100),
 surname_employee varchar(100),
-date_birthday date,
 post varchar(100),
 empl_login varchar(100),
-empl_passw varchar(100)
+empl_passw varchar(100),
+salary double precision
 );
 
 create table sales
@@ -48,29 +72,46 @@ id_sales int Identity(1,1) primary key,
 date_sales date,
 id_product int,
 ammount int,
-id_employe int,
 sum_sales double precision
 );
 
+create table categories
+(
+id_cat int primary key,
+name_cat varchar(100)
+);
 
-drop procedure addNewEmployee;
+create table delivery
+(
+id_delivery int  Identity(1,1) primary key,
+id_producer int,
+id_employee int,
+ammount int,
+sum_delivery double precision
+);
+
 go
 create procedure addNewEmployee
 (
 @name_employee varchar(100),
 @surname_employee varchar(100),
-@date_birthday date,
 @post varchar(100),
 @log varchar(100),
-@pass varchar (100)
+@pass varchar (100),
+@salary double precision
 )
 as
-insert into employee (name_employee,surname_employee,date_birthday, post,empl_login,empl_passw)
-values (@name_employee, @surname_employee, @date_birthday,@post, @log, @pass);
+insert into employee (name_employee,surname_employee, post,empl_login,empl_passw, salary)
+values (@name_employee, @surname_employee,@post, @log, @pass, @salary);
+
+select * from employee;
+select * from products;
+drop procedure selectProducts;
+
+create procedure selectProducts
+as select id_product, name_product, price, description_product,ammount, (select name_cat from categories where products.id_cat = categories.id_cat), (select name_producer from producer where products.id_producer = producer.id_producer) from products;
 
 select * from employee;
 
-create procedure selectProducts
-as select name_product, price, description_product from products;
-
-select * from employee
+create procedure selectEmployee
+as select id_employee, name_employee, surname_employee, post, empl_login, empl_passw, salary from employee;
