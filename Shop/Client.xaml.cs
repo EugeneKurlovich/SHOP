113848cffb833598aa0b680ceed208b26a0dce3d
EugeneKurlovich <zhenikpggkurlovich@gmail.com>
2017-12-17 22:27:15 +0300

    added client exceptions

--- Shop/Client.xaml.cs
+++ Shop/Client.xaml.cs
@@ -24,23 +24,37 @@ namespace Shop
         string connStr = @"Data Source=EUGENEPC;Initial Catalog=StoreDB;User Id = client; Password = qwe123";
         private void button_Click(object sender, RoutedEventArgs e)
         {
-            DB db = new DB();
-            db.openConnection(connStr);
-            db.getAllCategories();
-            cGrid.ItemsSource = null;
-            cGrid.ItemsSource = Categories.categoriesList;
-            MessageBox.Show("Выполнено !!!");
-            db.closeConnection();
+            try
+            {
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.getAllCategories();
+                cGrid.ItemsSource = null;
+                cGrid.ItemsSource = Categories.categoriesList;
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
         }
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
-            DB db = new DB();
-            db.openConnection(connStr);
-            db.getAllProducts();
-            pGrid.ItemsSource = ProductsList.prodList;
-            MessageBox.Show("Выполнено !!!");
-            db.closeConnection();
+            try
+            {
+                DB db = new DB();
+                db.openConnection(connStr);
+                db.getAllProducts();
+                pGrid.ItemsSource = ProductsList.prodList;
+                MessageBox.Show("Выполнено !!!");
+                db.closeConnection();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
         }
 
         private void button3_Click(object sender, RoutedEventArgs e)
@@ -51,26 +65,38 @@ namespace Shop
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-
-            ProductsList path = pGrid.SelectedItem as ProductsList;
-            DB db = new DB();
-            db.openConnection(connStr);
-            db.buySelectedProduct(path.id, Convert.ToInt32(textBox.Text));      
-            MessageBox.Show("Куплено");
-            db.closeConnection();
+            try
+            {
+                ProductsList path = pGrid.SelectedItem as ProductsList;
+                DB db = new DB();
+                db.buySelectedProduct(path.id, Convert.ToInt32(textBox.Text));
+                MessageBox.Show("Куплено");
+                db.closeConnection();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
         }
 
         private void button4_Click(object sender, RoutedEventArgs e)
         {
-            Categories path = cGrid.SelectedItem as Categories;
-            DB db = new DB();
-            db.openConnection(connStr);
-            ProductsList.prodList.Clear();
-            db.filtrProduct(path.id);
-            pGrid.ItemsSource = null;
-            pGrid.ItemsSource = ProductsList.prodList;
-            MessageBox.Show("Выполнено");
-            db.closeConnection();
+            try
+            {
+                Categories path = cGrid.SelectedItem as Categories;
+                DB db = new DB();
+                db.openConnection(connStr);
+                ProductsList.prodList.Clear();
+                db.filtrProduct(path.id);
+                pGrid.ItemsSource = null;
+                pGrid.ItemsSource = ProductsList.prodList;
+                MessageBox.Show("Выполнено");
+                db.closeConnection();
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка запроса");
+            }
         }
 
         private void button5_Click(object sender, RoutedEventArgs e)
